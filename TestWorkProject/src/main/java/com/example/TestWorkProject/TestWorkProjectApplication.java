e9b689ca6a5f03cda78c000ccb6efabeeb21d249
seropyh <69725611+seropyh@users.noreply.github.com>
2020-12-13 20:21:56 +0300

    For test with h2 database full project

---
+++ TestWorkProject/src/main/java/com/example/TestWorkProject/TestWorkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestWorkProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWorkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWorkProjectApplication.class, args);
+	}
+
+}
---
+++ TestWorkProject/src/test/java/com/example/TestWorkProject/TestWorkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestWorkProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWorkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
